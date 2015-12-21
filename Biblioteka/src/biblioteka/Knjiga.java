0472283c2b36bbe4280c4570d867bee39895f895
nikola <nikola@192.168.200.151>
2015-12-21 10:51:54 +0100

    V0.1 Implentirane stvari za osnovu knjiga

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -1,5 +1,60 @@
 package biblioteka;
 
+import java.util.LinkedList;
+
 public class Knjiga {
+	private String naslov;
+	private long ISBN;
+	private String izdavac;
+	private int izdavanje;
+	private LinkedList<Author> autori = new LinkedList();
+
+	public Knjiga(String naslov, long iSBN, String izdavac, int izdavanje) {
+		this.naslov = naslov;
+		ISBN = iSBN;
+		this.izdavac = izdavac;
+		this.izdavanje = izdavanje;
+	}
+
+	public Knjiga() {
+	}
+
+	public String getNaslov() {
+		return naslov;
+	}
+
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+
+	public long getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(long iSBN) {
+		ISBN = iSBN;
+	}
+
+	public String getIzdavac() {
+		return izdavac;
+	}
+
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+
+	public int getIzdavanje() {
+		return izdavanje;
+	}
+
+	public void setIzdavanje(int izdavanje) {
+		this.izdavanje = izdavanje;
+	}
+
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", ISBN=" + ISBN + ", izdavac=" + izdavac + ", izdavanje=" + izdavanje
+				+ "]";
+	}
 
 }
